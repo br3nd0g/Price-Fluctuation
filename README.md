c444b26554ba355df8962bf7f47a9a9b1cd867d0
brendan :) <brendan.j.corcoran@gmail.com>
2022-09-30 16:50:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Price Fluctuator
+
+A small program I made quickly, inspired by a problem in a project one of my friends is creating. 
+
+It takes an initial value (which i set as 50) and 'simulates' a fluctuation of the price, such as to mimic the stock market or such.
+
+Randomly generates a value to add or subtract, and also has some logic that uses a random number generator to make a 1/4 chance for it to switch direction (adding or subtracting)
+
+Really nothing special, but could be used for something interesting :)
---
+++ main.py
@@ -0,0 +1,50 @@
+# price fluctuation
+import random
+import time
+
+
+def nextStep(price, method):
+    #gets a two decimal point float to add or subtract
+    newAmount = round(random.uniform(0, 2.1), 2)
+
+    #adds or subtracts amount dependent on method
+    if method == "add":
+        price += newAmount
+    elif method == "subtract":
+        price -= newAmount
+
+    return price
+
+
+def fluctuate(price, dir):
+
+    #uses decided direction to decide method
+    dir = decideDirection(dir)
+    if dir == True: method = "add"
+    else: method = "subtract"
+
+    #adds or subtracts a random value
+    price = nextStep(price, method)
+
+    return price, dir
+
+
+def decideDirection(dir):
+    #uses a RNG to create a 1/4 chance of the direction switching
+    randNum = random.randint(0,4)
+
+    if randNum == 3:
+        if dir == True: dir = False
+        else: dir = True
+
+    return dir
+
+#initial values
+value = 50
+directionUp = True
+
+#while value and print statement to keep the 'simulation' going
+while True:
+    value, directionUp = fluctuate(value, directionUp)
+    print(value)
+    time.sleep(1)
